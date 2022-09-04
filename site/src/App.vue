b9293688c2644c7a8d5bb1b173c7fe3cf519f5d4
Serquand <esteban.vincent@efrei.net>
2022-09-05 01:00:42 +0200

    Setup

---
+++ site/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app-main-container">
+    <HeaderBar />
+    <Landing />
+    <Services />
+    <Formation />
+    <FooterBar />
+  </div>
+</template>
+
+<script>
+import HeaderBar from './components/HeaderBar.vue'
+import FooterBar from './components/FooterBar.vue';
+import Services from './components/Services.vue'
+import Formation from './components/Formation.vue'
+import Landing from './components/Landing.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderBar, 
+    FooterBar, 
+    Services, 
+    Formation, 
+    Landing
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ site/src/components/HeaderBar.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>HeaderBar</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
